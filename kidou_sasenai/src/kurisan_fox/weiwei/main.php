26b55faeac99fd5e38b0cc278f92e444f8105da4
KuriSan_Fox <78953997+KuriSan-Fox@users.noreply.github.com>
2021-02-14 00:14:44 +0900

    Update main.php
    コピーしてたコードがなぜか貼られてたので削除

--- kidou_sasenai/src/kurisan_fox/weiwei/main.php
+++ kidou_sasenai/src/kurisan_fox/weiwei/main.php
@@ -12,33 +12,10 @@ class Main extends PluginBase
 {
     public function onEnable()
     {
-	// THE 練習 のコード。
-        //while (true) {
-        //    $num1 = 8091867987493326313 ;
-        //    $num2 = 2151901553968352745  ;
-        //    $c = $num1 - $num2;
-        //    $this->getLogger()->notice('§czettai kidou sasenai kedo keisan sita yatsu dake ageru!:§a '.$c.' §cdayo!');
-        //}
         $this->getLogger()->notice('§aPlugin loaded!');
-        $this->getLogger()->notice('§chey,wait.');
-        $this->getLogger()->notice('HAHAHAHA!!!!!!!! YOU CAN NOT START SERVER!');
     }
     public function onDisable()
     {
         $this->getLogger()->notice("§cUnloaded Plugin! :)");
     }
-}
-
-class ExamplePlugin implements Plugin{
-	private $api;
-	public function __construct(ServerAPI $api, $server = false){
-		$this->api = $api;
-	}
-	
-	public function init(){
-	}
-	
-	public function __destruct(){
-	}
-
 }
