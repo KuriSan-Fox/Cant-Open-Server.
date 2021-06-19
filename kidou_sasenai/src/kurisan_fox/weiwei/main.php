8795c2853507d6ffe951f47677e1a496778fc16c
KuriSan_Fox <christophershefox@gmail.com>
2021-06-19 23:57:19 +0900

    書き直し
    今までJSみたいな書き方してるせいでPHPに対応してない書き方してたりしてエラー吐いてたので、main.phpを書き直しました。

--- kidou_sasenai/src/kurisan_fox/weiwei/main.php
+++ kidou_sasenai/src/kurisan_fox/weiwei/main.php
@@ -4,19 +4,47 @@ namespace kurisan_fox\weiwei;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase
+
 {
     public function onEnable()
     {
-        /*
-        $time = 0;
-        $time_max = 11451481019193643644545;
-        while ($time < $time_max) {
-            $this->getLogger()->notice('LOL!');   
-        }*/
-        $this->getLogger()->notice('§aPlugin has loaded now!');
-        $stopserver = 1;
-        while (stopserver == 1) {
-            $this->getLogger()->notice('I STOP START SERVER. HAHAHAHAHA!!!!');
+        $this->getLogger()->notice("§aLoading Plugin Baka Plugin...");
+        $fakeloadingbar = 0;
+        while($fakeloadingbar<100){
+            $fakeloadingbar = ($fakeloadingbar + 0.25);
+            $wait = 0;
+            while($wait <= 25){
+                $wait = $wait + 1;
+            }
+            if($fakeloadingbar > 100){
+                $fakeloadingbar = 100;
+            }
+            $this->getLogger()->notice("Loading Plugins...: $fakeloadingbar");
+        }
+        $this->getLogger()->notice("§aEND OF THREAD...");
+        $wait = 0;
+        while($wait < 10000000){
+            $wait = ($wait + 1);
+        }
+        $this->getLogger()->notice("Oh no, You have Server Blocker.");
+        $this->getLogger()->notice("Sorry, I Don't like this Server.");
+        $this->getLogger()->notice("Hmmm Please wait, Don't Worry.");
+        $wait = 0;
+        while($wait < 25100000){
+            $wait = ($wait + 1);
+        }
+        $this->getLogger()->notice("Ok, LOGGGGG");
+        while($wait< 2500000){
+            $wait = ($wait + 1);
+        }
+        // １秒に１メガバイトくらい増えるところです。
+        // 史上最悪のプラグイン。
+        $wait = 0;
+        $dostopserver = 1;
+        $timelost = 0.5;
+        while($dostopserver==1){
+            $timelost = ($timelost + 0.0001);
+            $this->getLogger()->notice("TIME: $timelost");
         }
     }
     public function onDisable()
