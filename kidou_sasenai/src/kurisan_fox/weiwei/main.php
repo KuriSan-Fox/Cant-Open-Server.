c3acfe7714e3191a7c774c683f685f65c8067450
ْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْْ <78953997+KuriSan-Fox@users.noreply.github.com>
2021-06-19 18:15:02 +0900

    ちょっと変更

--- kidou_sasenai/src/kurisan_fox/weiwei/main.php
+++ kidou_sasenai/src/kurisan_fox/weiwei/main.php
@@ -1,12 +1,4 @@
 <?php
-/*
-最初で最後のコードです。
-何か問題があれば消します。
-
-自由に使ってください。
-
-それとただの糞プラグインなので自由に編集してokです。。
-*/
 namespace kurisan_fox\weiwei;
 
 use pocketmine\plugin\PluginBase;
@@ -15,12 +7,17 @@ class Main extends PluginBase
 {
     public function onEnable()
     {
+        /*
         $time = 0;
         $time_max = 11451481019193643644545;
         while ($time < $time_max) {
             $this->getLogger()->notice('LOL!');   
-        }
+        }*/
         $this->getLogger()->notice('§aPlugin has loaded now!');
+        $stopserver = 1;
+        while (stopserver == 1) {
+            $this->getLogger()->notice('I STOP START SERVER. HAHAHAHAHA!!!!');
+        }
     }
     public function onDisable()
     {
